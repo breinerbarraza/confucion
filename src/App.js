e9211a5a266f0e1247269a2580239a301f499646
Breiner Barraza <breinerbarraza22@gmail.com>
2021-10-08 16:20:33 -0500

    rest

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent';
+import { PLATOS } from './shared/platos';
+
 class App extends Component{
+
+  constructor(props){
+      super(props);
+
+      this.state={
+        platos: PLATOS
+      };
+  }
+
   render(){
     return(
-      <div className="App">
+      <div >
             <Navbar dark color="primary">
                 <div className="container">
                   <NavbarBrand href="/">Ristorante Con Funcion</NavbarBrand>
                   
                 </div>
             </Navbar>
-            <Menu/>
+            <Menu  platos={this.state.platos}/>
       </div>
     )
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,79 +1,62 @@
 import React, {Component} from "react";
-import { Media } from 'reactstrap';
+//import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody } from 'reactstrap';
 
 class Menu extends Component {
 
     constructor (props){
         super(props);
-
         this.state = {
-            dishes:[
-                {
-                    id:0,
-                    name:'Uthappizza',
-                    image:'assets/images/uthappizza.png',
-                    category:'mains',
-                    label:'Hot',
-                    price:'4.99',
-                    desciption:'Plato de los mejores'
-                },
-                {
-                    id:1,
-                    name:'Zucchi',
-                    image:'assets/images/zucchipakoda.png',
-                    category:'Zucchi',
-                    label:'',
-                    price:'1.99',
-                    desciption:'Plato de los mejores',
-                },
-                {
-                    id:2,
-                    name:'Vadonunt',
-                    image:'assets/images/vadonut.png',
-                    category:'Vadonunt',
-                    label:'',
-                    price:'1.89',
-                    desciption:'Plato de los mejores',
-                },
-                {
-                    id:3,
-                    name:'Elaicheesecake',
-                    image:'assets/images/elaicheesecake.png',
-                    category:'Elaicheesecake',
-                    label:'',
-                    price:'1.333',
-                    desciption:'Plato de los mejores',
-                }     
-            ]
+            selectedDish: null
         }
+
+    }
+
+    onDishSelect(plato){
+            this.setState({selectedDish: plato})
+    }
+
+    renderDish(plato){
+        if(plato != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={x.image} alt={x.name} />
+                    <CardBody>
+                        
+                    </CardBody>
+                </Card>
+            )
+        }else{
+            return(
+                <div>
+
+                </div>
+            )
+        }
+
     }
     render(){
-        const menu = this.state.dishes.map((x)=>{
+        const menu = this.props.platos.map((x)=>{
             return(
-                <div key={x.id} className="col-12 mt-5">
-                        <Media tag="li">
-                            <Media left middle>
-                                <Media object src={x.image} alt={x.name} />
-                            </Media>
-                            <Media body className="ml-5">
-                                <Media heading>{ x.name }</Media>
-                                <p>{ x.desciption }</p>
-                            </Media>
-                        </Media>
+                <div key={x.id} className="col-12 col-md-5 m-1">
+                        <Card onClick={()=>this.onDishSelect(plato)}>
+  
+                                <CardImg width="100%" src={x.image} alt={x.name} />
+
+                            <CardImgOverlay >
+                                <CardTitle>{ x.name }</CardTitle>
+                            </CardImgOverlay>
+                        </Card>
                 </div>
             );
         } );
-        
         return(
             <div className="container">
                 <div className="row">
-                        <Media list >
-                                {menu}
-                        </Media>
+                        {menu}
                 </div>
             </div>
         );
     }
 }
-
 export default Menu ;
